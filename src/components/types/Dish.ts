98f7d4635c0177388660314dd8e3df6ae50f5c71
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-08-19 15:29:39 +0100

    adds custom types Dish & Comment

---
+++ src/components/types/Dish.ts
@@ -0,0 +1,20 @@
+export interface Comment {
+    id:      number;
+    rating:  number;
+    comment: string;
+    author:  string;
+    date:    Date;
+}
+
+interface Dish {
+    id:          number;
+    name:        string;
+    image:       string;
+    category:    string;
+    label:       string;
+    price:       string;
+    description: string;
+    comments:    Comment[];
+}
+
+export default Dish
